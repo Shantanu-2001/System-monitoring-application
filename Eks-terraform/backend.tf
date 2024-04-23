7f5cee333c0dd2205bad4146c6b70846c39a18b6
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2024-04-23 18:47:21 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bucket-for-eks" # Replace with your actual S3 bucket name
+    bucket = "System_Monitoring-bucket-for-eks" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
